648bf385919ac0a46df3d4de1e26801fb28327cf
zzz <906229256@qq.com>
2020-04-15 12:22:30 +0800

    change app

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -5,11 +5,13 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }, {
+    path: '/index',
+    name: 'index',
+    component: () => import('@/views/index')
+  }]
 })
---
+++ app/src/views/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>123</h3>
+    <span>4546</span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+div {
+  border: 1px solid red;
+  width: 300px;
+  height: 400px;
+  h3 {
+    border: 1px solid blue;
+  }
+  span {
+    border: 1px solid green;
+  }
+}
+</style>
